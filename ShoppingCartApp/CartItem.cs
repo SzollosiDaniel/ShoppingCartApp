f8df9e3eaa259ea1ac6193cedbfd503620322733
szdan <szdan@DANI_POWERPC>
2026-05-18 22:24:36 +0200

    CartItem implementálva

--- ShoppingCartApp/CartItem.cs
+++ ShoppingCartApp/CartItem.cs
@@ -4,24 +4,30 @@ namespace ShoppingCartApp
     {
         public string Name { get; }
         public double UnitPrice { get; }
-        public int Quantity { get; private set; }
+        public int Quantity { get; set; }
 
         // name nem lehet null/üres, unitPrice > 0, quantity >= 1
         public CartItem(string name, double unitPrice, int quantity)
         {
-            throw new NotImplementedException();
+            if (String.IsNullOrEmpty(name) || 0 >= unitPrice || 0 >= quantity)
+                throw new ArgumentException();
+            Name = name;
+            UnitPrice = unitPrice;
+            Quantity = quantity;
         }
 
         // UnitPrice * Quantity
         public double GetLineTotal()
         {
-            throw new NotImplementedException();
+            return UnitPrice * Quantity;
         }
 
         // quantity >= 1, különben ArgumentException
         public void UpdateQuantity(int quantity)
         {
-            throw new NotImplementedException();
+            if (0 >= quantity)
+                throw new ArgumentException();
+            Quantity = quantity;
         }
     }
 }
